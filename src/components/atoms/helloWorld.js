a4bd7869337c9d69ca561768aa2cae4e16797472
Dzianis Pytliak <azot952@gmail.com>
2021-03-28 01:33:18 +0300

    first commit

---
+++ src/components/atoms/helloWorld.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const HelloWorld = ({name}) => <Text>TESSS World {name}!</Text>;
+
+export default HelloWorld;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {HelloWorld} from '_atoms';
+import Navigator from '_navigations';
+// const App = () => <HelloWorld name="Helder Burato Berto" />;
+const App = () => <Navigator />;
+// const App = () => (
+//   <View>
+//     <Text>Hello fWsdfsdorld</Text>
+//   </View>
+// );
+export default App;
