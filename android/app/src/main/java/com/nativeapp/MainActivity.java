168f7319a69b20c4d657f4e7539acb29cc04e97d
Dzianis Pytliak <azot952@gmail.com>
2021-03-28 11:04:09 +0300

    rename project

--- android/app/src/main/java/com/nativeapp/MainActivity.java
+++ android/app/src/main/java/com/nativeapp/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "NativeApp";
+    return "StarterReactNativeApp";
   }
 }
